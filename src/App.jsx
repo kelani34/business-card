5f230a1ba210fa560dfa2c86d44543f56e96c4f7
kelani34 <kelanitaiwo34@gmail.com>
2022-09-24 20:33:00 +0100

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,6 @@ import Interest from './Interest'
 import Footer from './Footer'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className='container'>
       <Info />
